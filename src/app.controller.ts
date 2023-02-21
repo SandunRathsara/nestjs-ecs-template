66bc23671b1e38c3ffb2761e3bbee8cb025a844f
Sandun Rathsara <sandun@surge.global>
2023-02-21 13:03:08 +0530

    Dockerized and Buildspec added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/test')
+  testRoute(): string {
+    return 'This is a test route';
+  }
 }
